cce0b213aeb16598a0f9bde498839922a806eceb
khanhTran <khanhtrantung190894@gmail.com>
2021-02-25 23:38:08 +0700

    add model

---
+++ lib/model/image_item.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+class ImageItem {
+  final File image;
+  final int itemId;
+  ImageItem({this.image, this.itemId});
+}
---
+++ lib/model/type.dart
@@ -0,0 +1,7 @@
+class Type {
+  final int id;
+  final String name;
+  final String description;
+
+  Type({this.id, this.name, this.description});
+}
