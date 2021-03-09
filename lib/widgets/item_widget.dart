4dd609d6223e68d707eadbb7b6fcdb271adcbae6
khanhTran <khanhtrantung190894@gmail.com>
2021-03-09 23:48:30 +0700

    add lib, show 1 item

--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../model/item.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class ItemWidget extends StatelessWidget {
   final Item item;
@@ -7,10 +8,33 @@ class ItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> urlImg = [
+      'http://via.placeholder.com/210x320',
+      'http://via.placeholder.com/210x320',
+      'http://via.placeholder.com/210x320',
+      'http://via.placeholder.com/210x320'
+    ];
     return Container(
       child: Column(
         children: [
-          Image.network('http://via.placeholder.com/200x320'),
+          Container(
+            width: 200,
+            height: 320,
+            color: Colors.amber,
+            child: CarouselSlider.builder(
+              options: CarouselOptions(
+                autoPlay: false,
+              ),
+              itemCount: urlImg.length,
+              itemBuilder: (BuildContext context, int itemIndex, _) =>
+                  Container(
+                child: Image.network(
+                  urlImg[itemIndex],
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
           Text('name item')
         ],
       ),
